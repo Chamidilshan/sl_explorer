c205443f5c5bcd3cf98b0a231232668ebbf9f28b
Denuwan Sankalpa <denuwansankalpa123@gmail.com>
2024-01-27 11:02:02 +0530

    added CcruiseShip class

---
+++ lib/features/home/cruise_ships/cruiseship_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CruisShip extends StatefulWidget {
+  const CruisShip({super.key});
+
+  @override
+  State<CruisShip> createState() => _CruisShipState();
+}
+
+class _CruisShipState extends State<CruisShip> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
